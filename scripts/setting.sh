6d87f9b814d3f46e7187d61bd1304be846222826
cjg9040 <chl8273@naver.com>
2020-05-09 08:07:49 +0900

    ci/cd script modify

--- scripts/setting.sh
+++ scripts/setting.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sudo touch /home/ubuntu/airtxt/ucut/ip
-sudo chmod 766 /home/ubuntu/airtxt/ucut/ip
+sudo touch /home/ubuntu/server/ucut/ip
+sudo chmod 766 /home/ubuntu/server/ucut/ip
 sudo ifconfig | grep "inet" | awk '{print $2}'| head -n 1| sed -e 's/addr://g' > /home/ubuntu/server/ucut/ip
 sudo chmod 755 /home/ubuntu/server/ucut/ip
-sudo cp /home/ubuntu/airtxt/airtxt/ip /home/ubuntu/server/ucut/ip
+sudo cp /home/ubuntu/server/ucut/ip /home/ubuntu/server/ucut/ip
 
 sudo chown www-data:www-data /home/ubuntu/server/db.sqlite3
 sudo chown www-data:www-data /home/ubuntu/server
 
 directory="/home/ubuntu/development"
-source /home/ubuntu/airtxt/venv/bin/activate
-sudo python3 /home/ubuntu/airtxt/manage.py makemigrationsrl
+source /home/ubuntu/server/venv/bin/activate
+sudo python3 /home/ubuntu/server/manage.py makemigrationsrl
 
 if [ -d "$directory" ]; then
     sudo cp /home/ubuntu/development/server/wsgi.py /home/ubuntu/server/ucut/wsgi.py
@@ -18,8 +18,8 @@ if [ -d "$directory" ]; then
     sudo python3 /home/ubuntu/server/manage.py migrate
     echo "File $directory exists."
 else
-    sudo python3 /home/ubuntu/airtxt/manage.py migrate --database=auth_master
-    sudo python3 /home/ubuntu/airtxt/manage.py migrate --database=shorturl_master
+    sudo python3 /home/ubuntu/server/manage.py migrate --database=auth_master
+    sudo python3 /home/ubuntu/server/manage.py migrate --database=shorturl_master
     echo "File $directory does not exist."
 fi
 
