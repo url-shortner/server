9364b46a110bab29d5756d1d53285bfca60dab0c
bell2lee <paperlee@kookmin.ac.kr>
2020-05-19 01:17:06 +0900

    server-6 tenping 재생광고

--- shorturl/tenping.py
+++ shorturl/tenping.py
@@ -2,16 +2,36 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 class Tenping:
+    apiUrl = "https://tenping.kr"
+
     def __init__(self, id, pw):
         self.session = requests.session()
-        res = self.session.post('https://tenping.kr/Member/ActionLogin',
+        res = self.session.post(self.apiUrl + '/Member/ActionLogin',
                                 {"MemberID": id, "Password": pw, "AutoLogin": "Y"}, verify=False)
         self.headers = {"Cookie": res.headers['Set-Cookie']}
+
+    def get(self, url, params=None):
+        return self.session.get(self.apiUrl + url, headers=self.headers, params=params)
+
     def getInfo(self):
-        html = self.session.get("https://tenping.kr/Setting/ModifyInfo", headers=self.headers)
-        soup = bs(html.text, 'html.parser')
+        html = self.get("/Setting/ModifyInfo")
+        soup = bs(html.text, "html.parser")
         return {"NickName": soup.select('#content #NickName')[0]["value"],
                 "Member_Mobile": soup.select('#content #Member_Mobile')[0]["value"]}
+    def playAd_list(self):
+        query_parameter = {"Campaign_Category": 0,
+                   "CampaignType": 288,
+                   "FavoriteStatus": 8702}
+        html = self.get("/Home/List", params=query_parameter)
+        soup = bs(html.text, "html.parser")
+        #select_list = soup.select(html.text, "#campaign-list")
+        print(soup)
+        # result = []
+        # for el in select_list:
+        #     result.append({
+        #         el.a["href"]
+        #     })
+        # return result
 
 
 
@@ -19,4 +39,7 @@ if __name__ == "__main__":
     tenping = Tenping("id", "pw")
     user_profile = tenping.getInfo()
     print(user_profile)
+    pl = tenping.playAd_list()
+    print(pl)
+
 
