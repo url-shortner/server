4ee22e75c4696daf66d1c46d7abfc52b960ff231
bell2lee <paperlee@kookmin.ac.kr>
2020-05-22 18:07:38 +0900

    Update getPlayList function for category #6

--- shorturl/tenping.py
+++ shorturl/tenping.py
@@ -18,6 +18,7 @@ class Tenping:
         soup = bs(html.text, "html.parser")
         return {"NickName": soup.select('#content #NickName')[0]["value"],
                 "Member_Mobile": soup.select('#content #Member_Mobile')[0]["value"]}
+
     def getPlayList(self):
         query_parameter = {"Campaign_Category": 0,
                    "CampaignType": 288,
@@ -31,47 +32,10 @@ class Tenping:
             result.append({
                 "title": tmp.select_one(".list-tit").text.strip(),
                 "price": tmp.select_one("ul:nth-child(5) > li:nth-child(1)").text.strip(),
-                "category": {"location": tmp.select_one("a > dl > dd.txt-nation.tooltipped.tooltipped-n")["aria-label"]},
+                "category": {
+                    "location": tmp.select_one("a > dl > dd.txt-nation.tooltipped.tooltipped-n")["aria-label"],
+                    "method": tmp.select_one("dl > dd:nth-child(6)").text,
+                },
                 "url": tmp.select_one("a")["href"],
             })
         return result
-'''
-
-var nation_code = "";
-            var campaign_Category = "0";
-            var orderType = "8001";
-            var campaign_Type = "1153";
-            var searchWord = "";
-            var memberID = "2017042823510015";
-            var currentPage = $("#hdCurrentPage").val();
-            var pageSize = $("#hdPageSize").val();
-            var favoriteStatus = $("#hdFavoriteStatus").val();
-            var mdStatus = $("#hdMDStatus").val();
-            var popularity_Date = $("#hdPopularity_Date").val();
-            var strSort_name = "8001";
-
-            $.ajax({
-                url: "/Home/CampaignList",
-                type: "POST",
-                data: { "Nation_Code": nation_code, "Campaign_Category": campaign_Category, "OrderType": orderType, "SearchWord": searchWord, "MemberID": memberID, "CurrentPage": currentPage, "PageSize": pageSize, "CampaignType": campaign_Type, "FavoriteStatus": favoriteStatus, "MDStatus": mdStatus, "Popularity_Date": popularity_Date, "SortName": strSort_name },
-                dataType: "html",
-                success: function (data) {
-                    var liLength = $("#campaign-list>li").length;
-                    var divLength = $("#campaign-list>div.list-cont").length;
-                    console.log(liLength + " / " + divLength);
-                    if (data.indexOf("txt-nodata") != -1) {
-
-                        //데이터가 없을 경우 스크롤 N, 검색어가 있을 경우 [검색 결과가 없습니다. ] 노출
-                        //$("#hdScrollYN").val("N");
-                        //$("#campaign-list").after("<p class=\"txt-nodata s1\">ALERT_검색된_결과가_없습니다</p>");
-                        if (liLength == 0 && divLength == 0) {
-                            $("#campaign-list").append(data);
-                        }
-                        $("#divSpinner").hide();
-                    } else {
-                        $("#hdScrollYN").val("Y");
-                        $("#divSpinner").hide();
-                        $("#campaign-list").append(data);
-                        totalData = totalData.concat(data);
-                    }
-'''
